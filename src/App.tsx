37a6c3c4df81b19313b4b18e122db2dee04d18d8
donmk <slleotta@gmail.com>
2024-02-25 16:24:47 +0800

    Adding a page measurer

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,26 @@ import { Card } from "./components/cards/card";
 import castle from "./img/castle.png";
 import { Footer } from "./components/footer/footer";
 import { LanguageLoaderProvider } from "./components/language_selector";
+import { PageProvider } from "./components/page_measurer/page_measurer";
 
 function App() {
   return (
     <LanguageLoaderProvider>
-      <Header />
-      <div className="article">
-        <main className="main">
-          <CardGrid>
-            <Card>Hi</Card>
-            <Card>Hi</Card>
-            <Card>Hi</Card>
-            <Card>Hi</Card>
-          </CardGrid>
-        </main>
-        <img src={castle} className="castle"></img>
-        <Footer />
-      </div>
+      <PageProvider>
+        <Header />
+        <div className="article">
+          <main className="main">
+            <CardGrid>
+              <Card>Hi</Card>
+              <Card>Hi</Card>
+              <Card>Hi</Card>
+              <Card>Hi</Card>
+            </CardGrid>
+          </main>
+          <img src={castle} className="castle"></img>
+          <Footer />
+        </div>
+      </PageProvider>
     </LanguageLoaderProvider>
   );
 }
