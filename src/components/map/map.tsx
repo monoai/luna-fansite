d66ff48d5a8916bb3da7427e688cd93ce5fcd595
Koneko <162368196+koneko88@users.noreply.github.com>
2024-03-07 05:58:13 +0000

    Integrating don's map marker icons change

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,25 +1,16 @@
-import classnames from "classnames";
-
 //imported styles and locations (later add icons)
 import styles from "./map.module.css";
 
 //downloaded packages link
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Tooltip,
-  useMapEvents,
-} from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-cluster";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 //leaflet defaults broke on me somehow.... this is completely unesssary unless its broken like me
-import L from "leaflet";
+import L, { Icon } from "leaflet";
 import iconUrl from "leaflet/dist/images/marker-icon.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
-import { useEffect, useState } from "react";
 import { UserPost } from "../../posts";
+import { getPfp } from "../../postAssets";
 
 L.Marker.prototype.options.icon = L.icon({
   iconUrl,
@@ -30,64 +21,30 @@ L.Marker.prototype.options.icon = L.icon({
   shadowSize: [41, 41],
 });
 
-const MyComponent = (props: { closeOverlay: () => void }) => {
-  useMapEvents({
-    click() {
-      props.closeOverlay();
-    },
-  });
-  return null;
-};
-
-var Overlay = (props: {
-  message?: string;
-  username?: string;
-  image?: string;
-  open: boolean;
-}) => (
-  <div
-    className={classnames(
-      styles.overlay_container,
-      props.open && styles.overlay_open,
-    )}
-  >
-    <h1>{props?.username}</h1>
-    <p>{props?.message}</p>
-    <div>{props.image && <img src={props.image} />}</div>
-  </div>
-);
-
-type OverlayInfo = {
-  open: boolean;
-  message?: string;
-  username?: string;
-};
+const img_root = process.env.PUBLIC_URL + "/user/img/";
 
 export const Map = (props: {
   posts: UserPost[];
   cardRefs: Map<string, React.RefObject<HTMLDivElement>>;
-  innerRef?: (
-    map: L.Map,
-    openOverlay: (info: Omit<OverlayInfo, "open">) => void,
-  ) => void;
+  innerRef?: (map: L.Map) => void;
 }) => {
-  const [overlayInfo, setOverlayInfo] = useState<{
-    open: boolean;
-    message?: string;
-    username?: string;
-  }>({ open: false });
-
   let markers = props.posts
     .filter((post) => post.location != null)
     .map((post: UserPost, i) => {
+      const icon = new Icon({
+        iconUrl: getPfp(post),
+        iconSize: [35, 35],
+        iconAnchor: [17, 17],
+        className: styles.map_icon,
+      });
       return (
         <Marker
           key={i}
+          icon={icon}
           position={[post.location!.lat, post.location!.long]}
           eventHandlers={{
             click: () => {
-              let cardDiv = props.cardRefs.get(post.id)!.current!;
-              let rect = cardDiv.getBoundingClientRect();
+              const cardDiv = props.cardRefs.get(post.id)!.current!;
               // cardDiv.parentElement!.scroll(0, rect.top + 80);
               cardDiv.scrollIntoView({
                 behavior: "smooth",
@@ -96,7 +53,7 @@ export const Map = (props: {
           }}
         >
           <Tooltip
-            offset={[15, -40]}
+            offset={[0, -22]}
             direction="center"
             permanent
             className={styles.numberIcon}
@@ -109,9 +66,7 @@ export const Map = (props: {
 
   return (
     <MapContainer
-      ref={(m) =>
-        props.innerRef?.(m!, (info) => setOverlayInfo({ ...info, open: true }))
-      }
+      ref={(m) => props.innerRef?.(m!)}
       className={styles.map}
       zoom={1.5}
       center={[30, 0]}
@@ -123,7 +78,6 @@ export const Map = (props: {
         [1200, Infinity],
       ]}
       inertia={true}
-      //(funny inertiaDeceleration = {1})
       inertiaDeceleration={4000}
       worldCopyJump={true}
     >
