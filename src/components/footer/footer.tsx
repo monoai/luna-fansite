13002b8ec0baa7fd4e2e9bfa6fab8b66b91304c6
Koneko <162368196+koneko88@users.noreply.github.com>
2024-03-09 10:13:13 +0000

    New header and footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,11 @@ import castle from "./img/castle.png";
 
 export const Footer = () => (
   <footer className={styles.pageFooter}>
-    <div className={styles.footerText}>Footer text here</div>
+    <div className={styles.footerText}>
+      Created with ❤️ by LuKnights of{" "}
+      <a href="https://discord.gg/cuteisjustice">Luna's Candy Kingdom</a>{" "}
+      Discord
+    </div>
     <img src={castle} className={styles.castle}></img>
   </footer>
 );
